9f5be7c8e3d87d7dd7836dd068910d86c5530c9f
Ran <rsheridan613@gmail.com>
2022-10-29 12:15:33 -0600

    added arrays

--- script.js
+++ script.js
@@ -7,8 +7,79 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//variables
+var lowerCaseArr = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCaseArr = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numbersArr = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialArr = ["!", "@", "#", "$", "*", "%"];
+
+//length prompt
+
+//lowercase prompt
+
+//uppercase prompt
+
+//number prompt
+
+//special prompt
+
+//
